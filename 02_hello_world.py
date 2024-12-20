ba4ec060be9c99c396b434b36b295d7066a7a17c
rodolfosanchezg <ing.rodolsan@gmail.com>
2024-12-20 10:37:33 -0500

    New file about variables and how to use print command

---
+++ 02_hello_world.py
@@ -0,0 +1,10 @@
+#Using command print
+print("Hello Python world!")
+
+#Using a variable to print the same message
+message = "Hello Python world!"
+print(message)
+
+#Using same variable to print a different message
+message = "Hello Python world of learning!"
+print(message)
