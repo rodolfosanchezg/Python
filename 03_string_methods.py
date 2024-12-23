758fc881eaea9a9d9430a519a2073f7f466a5e35
rodolfosanchezg <ing.rodolsan@gmail.com>
2024-12-23 05:38:18 -0500

    String methods

---
+++ 03_string_methods.py
@@ -0,0 +1,12 @@
+#using .title() method
+name = "rodolfo sanchez"
+print(name)
+print(name.title())
+
+#using .upper() method
+name = "rodolfo sanchez"
+print(name.upper())
+
+#using .lower() method
+name = "RODOLFO SANCHEZ"
+print(name.lower())
