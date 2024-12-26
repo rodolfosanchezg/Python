ef43df97c1f97a5cfecbb5eee8ed97550000562d
rodolfosanchezg <ing.rodolsan@gmail.com>
2024-12-26 09:14:41 -0500

    Some additional examples

--- Cap04_TryItYourself.py
+++ Cap04_TryItYourself.py
@@ -100,40 +100,102 @@ print(cube)
 #lines to the end of the program that do the following:
 #• Print the message The first three items in the list are:. Then use a slice to
 #print the first three items from that program’s list.
+
+pizzas = ['margerita', 'napolitan', 'pepperoni', 'sea food', 'tropical', 'mexican']
+print("The first three items in the list are: ")
+for pizza in pizzas[:3]:
+    print(pizza.title())
+
 #• Print the message Three items from the middle of the list are:. Use a slice to
 #print three items from the middle of the list.
+
+pizzas = ['margerita', 'napolitan', 'pepperoni', 'sea food', 'tropical', 'mexican']
+print("Three items from the middle of the list are: ")
+for pizza in pizzas[1:4]:
+    print(pizza.title())
+
 #• Print the message The last three items in the list are:. Use a slice to print the
 #last three items in the list.
+
+pizzas = ['margerita', 'napolitan', 'pepperoni', 'sea food', 'tropical', 'mexican']
+print("The last three items in the list are: ")
+for pizza in pizzas[-3:]:
+    print(pizza.title())
+
 #4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1
 #(page 56). Make a copy of the list of pizzas, and call it friend_pizzas.
+
+pizzas = ['margerita', 'napolitan', 'pepperoni', 'sea food', 'tropical', 'mexican']
+friend_pizzas = pizzas[:]
+
 #Then, do the following:
 #• Add a new pizza to the original list.
+
+pizzas.append('peach and plums')
+
 #• Add a different pizza to the list friend_pizzas.
+
+friend_pizzas.append('beacon and tomato')
+
 #• Prove that you have two separate lists. Print the message My favorite
 #pizzas are:, and then use a for loop to print the first list. Print the message
 #My friend’s favorite pizzas are:, and then use a for loop to print the second
 #list. Make sure each new pizza is stored in the appropriate list.
+
+print("My favorite pizzas are: ")
+for pizza in pizzas[:]:
+    print(pizza.title())
+
+print("My friend's favorite pizzas are: ")
+for pizza in friend_pizzas[:]:
+    print(pizza.title())
+
 #4-12. More Loops: All versions of foods.py in this section have avoided using
 #for loops when printing to save space. Choose a version of foods.py, and
 #write two for loops to print each list of foods.
 
+my_foods = ['pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream']
+print("\nMy favorite food are: ")
+for food in my_foods[:]:
+    print(food.title())
+
 #4-13. Buffet: A buffet-style restaurant offers only five basic foods. Think of five
 #simple foods, and store them in a tuple.
+
+food_menu = ('parmesan cheese', 'steak and eggs', 'potato bacon', 'cereal and milk', 'buffalo wings')
+
 #• Use a for loop to print each food the restaurant offers.
+
+print("\nToday's menu is as follows:")
+for food in food_menu:
+    print(food.title())
+
 #• Try to modify one of the items, and make sure that Python rejects the
 #change.
+
+#food_menu[0] = 'mushrooms soup'
+
 #• The restaurant changes its menu, replacing two of the items with different
 #foods. Add a line that rewrites the tuple, and then use a for loop to print
 #each of the items on the revised menu.
 
+food_menu = ('mushroom soup', 'chaufa rice', 'potato bacon', 'buffalo wings', 'steak and eggs')
+print("\nThe new menu is as follows: ")
+for food in food_menu:
+    print(f"* {food.title()}")
+
 #4-14. PEP 8: Look through the original PEP 8 style guide at https://python.org/
 #dev/peps/pep-0008/. You won’t use much of it now, but it might be interesting
 #to skim through it.
+
 #4-15. Code Review: Choose three of the programs you’ve written in this chapter
 #and modify each one to comply with PEP 8:
+
 #• Use four spaces for each indentation level. Set your text editor to insert
 #four spaces every time you press tab, if you haven’t already done so (see
 #Appendix B for instructions on how to do this).
+
 #• Use less than 80 characters on each line, and set your editor to show a
 #vertical guideline at the 80th character position.
+
 #• Don’t use blank lines excessively in your program files.
